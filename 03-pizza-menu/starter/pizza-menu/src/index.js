7fd3ada977a4f01b7974144620df715e50a967f3
Kristof1992 <kristof92selecz@gmail.com>
2023-08-09 17:20:25 +0200

    Working w Components - Destructuring props

--- 03-pizza-menu/starter/pizza-menu/src/index.js
+++ 03-pizza-menu/starter/pizza-menu/src/index.js
@@ -100,52 +100,47 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
-  console.log(props);
-
-  if (props.pizzaObj.soldOut) return null;
+// Immediately destructuring props
+function Pizza({ pizzaObj }) {
+  if (pizzaObj.soldOut) return null;
 
   return (
     <div className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price + 3}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price + 3}</span>
       </div>
     </div>
   );
 }
 
-function Footer() {
+function Footer(props) {
   const hour = new Date().getHours();
   const openHour = 12;
   const closeHour = 22;
-
   const isOpen = hour >= openHour && hour <= closeHour;
-
-  // We can have multiple returns
-  // if (!isOpen) {
-  //   return (
-  //     <p>
-  //       XXXWe are happy to welcome you between {openHour}:00 and {closeHour}:00.
-  //     </p>
-  //   );
-  // }
 
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We are happy to welcome you between {openHour}:00 and {closeHour}:00.
         </p>
       )}
     </footer>
+  );
+}
+
+function Order({ openHour, closeHour }) {
+  return (
+    <div className="order">
+      <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+      <button className="btn">Order</button>
+    </div>
   );
 }
 
