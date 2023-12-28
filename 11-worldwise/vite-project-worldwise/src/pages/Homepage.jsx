046e42d43a8ab691736bd58d3b6a36c7a1bf109c
Kristof1992 <kristof92selecz@gmail.com>
2023-12-28 13:53:38 +0100

    React Router - SPA CSS Modules

--- 11-worldwise/vite-project-worldwise/src/pages/Homepage.jsx
+++ 11-worldwise/vite-project-worldwise/src/pages/Homepage.jsx
@@ -1,10 +1,14 @@
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
+import { Link } from "react-router-dom";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
-      <h1>WorldWise</h1>
+      <AppNav />
+      <h1 className="test">WorldWise</h1>
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
