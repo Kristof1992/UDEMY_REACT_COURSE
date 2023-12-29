03d14b842407c4c91761c6961f688f95b5e9ebdd
Kristof1992 <kristof92selecz@gmail.com>
2023-12-29 03:22:59 +0100

    React Router - SPA Nested Routes/Index Route

--- 11-worldwise/vite-project-worldwise/src/App.jsx
+++ 11-worldwise/vite-project-worldwise/src/App.jsx
@@ -5,15 +5,23 @@ import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 
+import Login from "./pages/Login";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />}></Route>
+        <Route index element={<Homepage />}></Route>
         <Route path="product" element={<Product />}></Route>
         <Route path="pricing" element={<Pricing />}></Route>
+        <Route path="/login" element={<Login />}></Route>
         <Route path="*" element={<PageNotFound />}></Route>
-        <Route path="app" element={<AppLayout />}></Route>
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of cities</p>}></Route>
+          <Route path="cities" element={<p>List of cities</p>}></Route>
+          <Route path="countries" element={<p>Countries</p>}></Route>
+          <Route path="form" element={<p>Form</p>}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
