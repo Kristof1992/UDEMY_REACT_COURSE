26f009ff0b9f9fa56ce5dd01dc9af8bb43bc664f
Kristof1992 <kristof92selecz@gmail.com>
2024-03-12 19:15:45 +0100

    Last Project - Server-Side Pag. Pre-Fetching

--- 17-the-wild-oasis/the-wild-oasis/src/features/bookings/useBookings.js
+++ 17-the-wild-oasis/the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,12 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 
 /**
  * Here we are fetching data on request. We are filtering the query instead of the local data.
  */
 export function useBookings() {
   const [searchParams] = useSearchParams();
+  const queryClient = useQueryClient();
   // FILTER
   const filterValue = searchParams.get("status");
   const filter =
@@ -22,6 +24,7 @@ export function useBookings() {
   // PAGINATION
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
+  // QUERY
   const {
     isLoading,
     data: { data: bookings, count } = {},
@@ -30,6 +33,23 @@ export function useBookings() {
     queryKey: ["bookings", filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+
+  console.log(bookings);
+
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+  // PRE - FETCHING;
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+
+  if (page > 1 && page < pageCount) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
+  }
 
   return { isLoading, error, bookings, count };
 }
