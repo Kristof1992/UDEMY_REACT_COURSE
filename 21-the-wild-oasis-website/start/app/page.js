d9c6fd8c28577a5348668f6d7909611815d62f74
Kristof1992 <kristof92selecz@gmail.com>
2024-10-12 06:07:09 +0200

    Overview of Next.js - Navigating Between Pages

--- 21-the-wild-oasis-website/start/app/page.js
+++ 21-the-wild-oasis-website/start/app/page.js
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>Hello Next!</h1>;
+  return (
+    <div>
+      <h1>The Wild Oasis. Welcome to paradise.</h1>
+
+      {/* Instead of <a> we use the <Link> component
+        * in order to have the Single App feel.
+        * More optimized behind the scenes.
+        *
+      <a href="/cabins">Explore luxury cabins</a> */}
+      <Link href="/cabins">Explore luxury cabins</Link>
+    </div>
+  );
 }
