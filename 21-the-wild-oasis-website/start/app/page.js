9bfb0db6c99f11fb318f6762ee8a211fa094979d
Kristof1992 <kristof92selecz@gmail.com>
2024-10-16 11:32:31 +0200

    Web Project w Next.js - Improved Styles, Metadata, Starter Components

--- 21-the-wild-oasis-website/start/app/page.js
+++ 21-the-wild-oasis-website/start/app/page.js
@@ -3,14 +3,7 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div>
-      <h1>The Wild Oasis. Welcome to paradise.</h1>
-
-      {/* Instead of <a> we use the <Link> component
-        * in order to have the Single App feel.
-        * More optimized behind the scenes.
-        *
-      <a href="/cabins">Explore luxury cabins</a> */}
-      <Link href="/cabins">Explore luxury cabins</Link>
+      <Link href="/cabins">Cabins page</Link>
     </div>
   );
 }
