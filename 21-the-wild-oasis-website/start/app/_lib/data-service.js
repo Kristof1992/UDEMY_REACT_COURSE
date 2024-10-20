7c1e879bb13e87dfcf059b815e4821292078c5c6
Kristof1992 <kristof92selecz@gmail.com>
2024-10-20 21:48:00 +0200

    Web Project w Next.js - Streaming UI With Suspense

--- 21-the-wild-oasis-website/start/app/_lib/data-service.js
+++ 21-the-wild-oasis-website/start/app/_lib/data-service.js
@@ -1,6 +1,21 @@
 import { eachDayOfInterval } from "date-fns";
-import supabase from "@/app/_lib/db/supabase";
+import supabase from "@/app/_lib/supabase";
 
+export const getCabins = async function () {
+  const { data, error } = await supabase
+    .from("cabins")
+    .select("id, name, maxCapacity, regularPrice, discount, image")
+    .order("name");
+
+  await new Promise((res) => setTimeout(res, 2000));
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+};
 /////////////
 // GET
 
